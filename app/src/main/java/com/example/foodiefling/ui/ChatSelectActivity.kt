44551baabc8f55300a6d52e0b94f38ac9e91369f
Conor <contieman007@gmail.com>
2024-11-11 17:53:28 -0600

    Upload of all initial messaging functionality files (xml,kt,gradle,libs)

---
+++ app/src/main/java/com/example/foodiefling/ui/ChatSelectActivity.kt
@@ -0,0 +1,76 @@
+package com.example.foddieflingmessaging
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class ChatSelectActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<ChatUser>
+    private lateinit var adapter: ChatUserAdapter
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.chat_activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        userList = ArrayList()
+        adapter = ChatUserAdapter(this, userList)
+        // Initialize the RecyclerView and set the adapter with the user list from firebase
+        userRecyclerView = findViewById(R.id.userRecyclerView)
+        userRecyclerView.adapter = adapter
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        //add the users to the list
+        mDbRef.child("user").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear() // Clear the list before adding new users
+                for (postSnapshot in snapshot.children){
+                    val currentUser = postSnapshot.getValue(ChatUser::class.java)
+                    if (mAuth.currentUser?.uid != currentUser?.uid){
+                        userList.add(currentUser!!)
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.btn_logout) {
+            mAuth.signOut()
+            val intent = Intent(this@ChatSelectActivity, Login::class.java)
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return true
+    }
+}
